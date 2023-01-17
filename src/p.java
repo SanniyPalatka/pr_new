1de8775594aaa2787d8ab1023f83dbe42dcc0f3d
Kirill Chizhkov <79374140+SanniyPalatka@users.noreply.github.com>
2023-01-17 15:25:23 +0300

    Update p.java

--- src/p.java
+++ src/p.java
@@ -1529,7 +1529,43 @@ public void reverseHalf(Queue<Integer> q) {
 }
 
 
+Отлично! Вот ваш метод isPalindrome на Java:
 
+public static boolean isPalindrome(Queue<Integer> q) {
+
+    Stack<Integer> s = new Stack<>();
+
+    int size = q.size();
+
+    for (int i = 0; i < size; i++) {
+
+        int n = q.remove();
+
+        q.add(n);
+
+        s.push(n);
+
+    }
+
+    for (int i = 0; i < size; i++) {
+
+        int n1 = q.remove();
+
+        int n2 = s.pop();
+
+        if (n1 != n2) {
+
+            return false;
+
+        }
+
+        q.add(n1);
+
+    }
+
+    return true;
+
+}
 
 
 
